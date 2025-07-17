1b97706fb110d3793b9f2515d92440f4f116b1c7
halerazxc <babykinder666@bk.ru>
2025-07-17 17:52:45 +0400

    added background image as component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import Navbar from './Navbar'
+import headerImg from '../assets/header_img.png';
 
 const Header = () => {
   return (
-    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{ backgroundImage: "url('/src/assets/header_img.png')" }} id='Header'>
+    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{ backgroundImage: `url(${headerImg})` }} id='Header'>
       <Navbar />
       <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white'>
         <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20'>Explore homes that fit your dreams</h2>
